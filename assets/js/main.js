48fca8839efb1b5d9793bed426532034d07c87ae
ChochiasG <josias04102003@gmail.com>
2023-10-23 13:55:33 -0700

    cuadricula 4x3 terminada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,71 @@
 
-let containerCartas = document.getElementById('container');
+let selecciones = [];
+let imagenes = [
+    'image_1.jpg',
+    'image_2.jpg',
+    'image_3.jpg',
+    'image_4.jpg',
+    'image_5.jpg',
+    'image_6.jpg',
+    'image_7.jpg',
+    'image_8.jpg',
+    'image_9.jpg',
+    'image_10.jpg',
+];
 
-const crearDivCartas = () => {
-
-    for( let i = 0; i < 12; i++ ) {
-        const divCarta = document.createElement('div');
-        divCarta.classList.add('div-carta');
-
-        containerCartas.appendChild(divCarta);
-    }
-}
-crearDivCartas();
-
-const crearMazo = () => {
-
-    let mazo = [];
-    for(let i = 0; i < 2; i++) {
-    
-        for(let j = 0; j < 6; j++) {
-    
-            const imgCarta = document.createElement('img');
-            imgCarta.src = `assets/img/image_${j + 1}.jpg`;
-            imgCarta.classList.add('carta');
-
-            mazo.push( imgCarta );
+const generarCuadricula = () => {
+    let container = document.getElementById('container');
+    let cartas = [];
+    selecciones = [];
+    for( let numCarta = 0; numCarta < 12; numCarta++ ){
+        cartas.push(`
+            <div class="div-carta" onclick="seleccionarCarta(${numCarta})" >
+                <div id="carta_${numCarta}" class="carta">
+                    <div id="trasera_${numCarta}" class="cara trasera">
+                        <img src="assets/img/${imagenes[0]}" alt="">
+                    </div>
+                    <div class="cara frontal">
+                        <i class="fa-solid fa-question"></i>
+                    </div>
+                </div>
+            </div>
+        `);
+        if( numCarta%2 === 1 ) {
+            imagenes.splice(0,1);
         }
     }
-    return mazo;
+    cartas.sort(() => Math.random() - 0.5);
+    container.innerHTML = cartas.join("");
 }
 
-const revolverCartas = (array) => {
-    const compareRandom = () => Math.random() - 0.5;
-    array.sort(compareRandom);
-}
-
-const agregarCartasDiv = () => {
-
-    let divCartas = document.querySelectorAll('.div-carta');
-    revolverCartas(mazo);
-
-    for( let i = 0; i < 12; i++ ) {
-        divCartas[i].append( mazo[i] );
+const seleccionarCarta = (numCarta) => {
+    let carta = document.getElementById(`carta_${numCarta}`);
+    if( carta.style.transform != "rotateY(180deg)" ) {
+        carta.style.transform = "rotateY(180deg)";
+        selecciones.push(numCarta);
+    }
+    if(selecciones.length == 2) {
+        deseleccionar(selecciones);
+        selecciones = [];
     }
 }
 
-let mazo = crearMazo();
-agregarCartasDiv();
+const deseleccionar = (selecciones) => {
+    setTimeout(() => {
+        let cartaUno = document.getElementById(`carta_${selecciones[0]}`);
+        let cartaDos = document.getElementById(`carta_${selecciones[1]}`);
+        let traseraUno = document.getElementById(`trasera_${selecciones[0]}`);
+        let traseraDos = document.getElementById(`trasera_${selecciones[1]}`);
+
+        if( traseraUno.innerHTML != traseraDos.innerHTML ) {
+            cartaUno.style.transform = "rotateY(0deg)";
+            cartaDos.style.transform = "rotateY(0deg)";
+        } else {
+            traseraUno.style.opacity = "50%";
+            traseraDos.style.opacity = "50%";
+        }
+    }, 800);
+}
+
+// LLamar a la funcion principal
+generarCuadricula();
